36142231b4c95549e2c2df278ab4278bab6b5934
Vũ Tuấn Đạt <tuanvutruonglacnhatsinh0801@gmail.com>
2023-12-08 23:39:15 +0900

    feat: radio button (2)

--- app/src/main/java/com/example/jcbasic/RadioCheckboxActivity.kt
+++ app/src/main/java/com/example/jcbasic/RadioCheckboxActivity.kt
@@ -5,15 +5,28 @@ import com.example.jcbasic.ui.theme.JetPackComposeBasicTheme
 import androidx.compose.ui.Modifier
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.selection.selectable
 import androidx.compose.material3.RadioButton
 import androidx.compose.material3.RadioButtonDefaults
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
@@ -70,9 +83,39 @@ fun PureRadioButton(){
 }
 
 @Composable
+fun RadioButtonWithText(){
+    var isSelected by remember {
+        mutableStateOf(false)
+    }
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier.selectable(
+            selected = isSelected,
+            onClick = { isSelected = !isSelected },
+            role = Role.RadioButton
+        )
+    ) {
+        RadioButton(
+            enabled = true,
+            selected = isSelected,
+            onClick = null,
+            colors = RadioButtonDefaults.colors(
+                selectedColor = Color.Red,
+                unselectedColor = Color.Black,
+                disabledSelectedColor = Color.Gray,
+            )
+        )
+        Text(text = "Manually", modifier = Modifier.padding(start = 15.dp) )
+        Text(text = " ${isSelected}" )
+    }
+}
+
+@Composable
 fun RadioCheckboxColumn(){
     Column(modifier = Modifier.padding(15.dp)){
-
+        PureRadioButton()
+        Spacer(modifier = Modifier.height(10.dp))
+        RadioButtonWithText()
     }
 }
 
